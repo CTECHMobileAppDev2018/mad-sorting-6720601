e8446c633950790a77070c41a35fa6578b8dd2dc
6720601 <46965875+6720601@users.noreply.github.com>
2019-02-26 08:24:29 -0600

    Bubble Sorting
    This is the code for sorting. I used GeeksForGeeks for help. You can find the exact page here: https://www.geeksforgeeks.org/bubble-sort/. I also got Bartucz's help.

--- Sort.java
+++ Sort.java
@@ -6,28 +6,30 @@ class Main
     // start and end are optional - some sorting algorithms use them, but you can ignore them
     void sort(double arr[], long start, long end) 
     { 
-      // sort the array here
+       int n = arr.length; 
+        for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (arr[j] > arr[j+1]) 
+                { 
+                    // swap arr[j+1] and arr[i] 
+                    double temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
     } 
 
   
     // Test program 
     public static void main(String args[]) 
     { 
-        double arr[] = {10.0, 7.1, 8.6, 9.9, 1.8, 5.0}; 
-        System.out.println("original array: "); 
+        double arr[] = {10.0, 7.1, -8.6, 9.9, 1.8, 5.0}; 
+        System.out.println("original array"); 
         System.out.println(Arrays.toString(arr));
         
-        // create a sorted version to test.
-        double sortedArr[] = arr.clone();
-        Arrays.sort(sortedArr);
-        System.out.println("sorted array: "); 
-        System.out.println(Arrays.toString(sortedArr));
-        
-       
         Main sorter = new Main(); // create a sorter object
         sorter.sort(arr, 0, arr.length); // call the sort function
   
-        System.out.println("*** your sorted array: "); 
+        System.out.println("sorted array"); 
         System.out.println(Arrays.toString(arr));
     } 
 } 
